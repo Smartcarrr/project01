3b87bb3af8f5456436064167b483a80d9984ad0b
smartcarrr <carr.ethan04@gmail.com>
2024-10-25 02:40:44 -0700

    Update

--- wordsearch.c
+++ wordsearch.c
@@ -1,13 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
 // Declarations of the two functions you will implement
 // Feel free to declare any helper functions or global variables
 void printPuzzle(char** arr);
 void searchPuzzle(char** arr, char* word);
 int bSize;
-
 // Main function, DO NOT MODIFY 	
 int main(int argc, char **argv) {
     if (argc != 2) {
@@ -16,21 +14,18 @@ int main(int argc, char **argv) {
     }
     int i, j;
     FILE *fptr;
-
     // Open file for reading puzzle
     fptr = fopen(argv[1], "r");
     if (fptr == NULL) {
         printf("Cannot Open Puzzle File!\n");
         return 0;
     }
-
     // Read the size of the puzzle block
     fscanf(fptr, "%d\n", &bSize);
     
     // Allocate space for the puzzle block and the word to be searched
     char **block = (char**)malloc(bSize * sizeof(char*));
     char *word = (char*)malloc(20 * sizeof(char));
-
     // Read puzzle block into 2D arrays
     for(i = 0; i < bSize; i++) {
         *(block + i) = (char*)malloc(bSize * sizeof(char));
@@ -40,7 +35,6 @@ int main(int argc, char **argv) {
         fscanf(fptr, "%c \n", *(block + i) + j);
     }
     fclose(fptr);
-
     printf("Enter the word to search: ");
     scanf("%s", word);
     
@@ -54,6 +48,32 @@ int main(int argc, char **argv) {
     return 0;
 }
 
+
+void upperCase(char* str) {
+    for (int i = 0; i < strlen(str); i++) {
+        if (*(str+i) >= 'a' && *(str+i) <= 'z') {
+            *(str+i) = *(str+i) - ('a' - 'A');
+        }
+    }
+}
+void showPath(int** path) {
+    for (int i = 0; i < bSize; ++i) {
+        for (int j = 0; j < bSize; ++j) {
+            int value =*(*(path+i)+j); //pointer notation
+            if (value != 0) {
+                int reversed = 0;
+                while (value > 0) {
+                    reversed = reversed * 10 + value % 10;
+                    value /= 10;
+                }
+                printf("%d\t", reversed);
+            } else {
+                printf("0\t");
+            }
+        }
+        printf("\n");
+    }
+}
 void printPuzzle(char** arr) {
 	// This function will print out the complete puzzle grid (arr). 
     // It must produce the output in the SAME format as the samples 
@@ -66,67 +86,70 @@ void printPuzzle(char** arr) {
 		printf("\n");
 	}
 	printf("\n");
-
 }
-
-void findNeighbor(char** arr, char* word, int i, int j){
-    int iMax = i+1, jMax = j+1;
-    int iMin = i-1, jMin = j-1;
-    int found = 0;
-    if(bSize < iMax){
-        iMax = bSize-1;
+int findNeighbor(char** arr, int** temp, char* word, int x, int y, int index) {
+    if (index == strlen(word) + 1) {
+        return 1;
     }
-    if(iMin < 0){
-        iMin = 0;
-    }
-    if(bSize <= jMax){
-        jMax = bSize-1;
-    }
-    if(jMin < 0){
-        jMin = 0;
-    }
-    for(int x = iMin; x <= iMax; x++){
-        if(found == 0){
-            for(int z = jMin; z <= jMax; z++){
-                if(*word == *(*(arr + x) + z)){
-                    findNeighbor(arr, word+1, x, z);
-                    if(*(word+1) == '\0'){
-                        printf("found");
-                        found =1;
-                        break;
+    for (int i = -1; i <= 1; i++) {
+        for (int j = -1; j <= 1; j++) {
+            int row = x + i;
+            int column = y + j;
+            if (row >= 0 && row < bSize && column >= 0 && column < bSize && *(*(arr + row) + column) == *(word + (index - 1))) {
+                if ((*(*(temp + row) + column) % 10 != index - 1 || *(*(temp + row) + column) == 0)) {
+                    int original = *(*(temp + row) + column);
+                    if (original == 0) {
+                        *(*(temp + row) + column) = index;
+                    } else {
+                        *(*(temp + row) + column) = original * 10 + index;
                     }
-            }
-        }
-        }
-        
-    }
-}
+                    
+                    if (findNeighbor(arr, temp, word, row, column, index + 1)){
+                        return 1;
+                    }
 
-void searchPuzzle(char** arr, char* word) {
-    // This function checks if arr contains the search word. If the 
-    // word appears in arr, it will print out a message and the path 
-    // as shown in the sample runs. If not found, it will print a 
-    // different message as shown in the sample runs.
-    // Your implementation here...
-    int z = 1;
-    char **pathArr = (char**)malloc(bSize * sizeof(char*));
-    for(int i = 0; i < bSize; i++){
-        *(pathArr + i) = (char*)malloc(bSize * sizeof(char));
-        for(int j = 0; j < bSize; j++){
-            *(*(pathArr + i) + j) = '0';
-        }
-    }
-
-    for(int i = 0; i < bSize; i++){
-        for(int j = 0; j < bSize; j++){
-            if(*word == *(*(arr + i) + j)){
-                findNeighbor(arr, word+1, i, j);
+                    if (original == 0) {
+                        *(*(temp + row) + column) = 0;
+                    } else {
+                        *(*(temp + row) + column) = (original - index) / 10;
+                    }
+                }
             }
         }
     }
-
     
-    printPuzzle(pathArr);
+    return 0;
+}
+void searchPuzzle(char** arr, char* word) {
+    int **temp = (int**)malloc(bSize * sizeof(int*));
+    for (int i = 0; i < bSize; i++) {
+        temp[i] = (int*)calloc(bSize, sizeof(int)); // Initialize to 0
+    }
+    upperCase(word);
+    int found = 0;
+    // Check for each cell if it can be the start of the word
+    for (int i = 0; i < bSize; i++) {
+        for (int j = 0; j < bSize; j++) {
+            if (*(*(arr+i)+j) == *(word+i)) { // Check if first letter matches
+                // Call findNeighbor and check if the word is found
+                if (findNeighbor(arr, temp, word, i, j, 1)) {
+                    found = 1; // Word found
+                }
+            }
+        }
+    }
+    // If any path was found, print it
+    if (found) {
+        printf("\nWord found!\nPrinting the search path:\n");
+        showPath(temp);
+    } else {
+        printf("Word not found!\n");
+    }
+    // Free allocated memory
+    for (int i = 0; i < bSize; i++) {
+        free(temp[i]);
+    }
+    free(temp);
 }
 
 
